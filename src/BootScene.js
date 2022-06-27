c9c3fd1a31403d927180e82f34f3cfaebdaaaa29
Havin Mickel <12345678900qw@gmail.com>
2022-06-27 08:31:39 -0700

    heroku 5

--- src/BootScene.js
+++ src/BootScene.js
@@ -1,13 +1,24 @@
 /** @format */
 
 import Phaser from 'phaser';
-import images from './assets/*.png';
+// import images from './assets/*.png';
 // import sounds from './sounds/*.mp3';
 import fall from './sounds/sounds_fall.mp3';
 import land from './sounds/land.mp3';
 import bump from './sounds/bump.mp3';
 import jump from './sounds/jump.mp3';
-
+import level1 from './assets/level1.png';
+import level2 from './assets/level2.png';
+import level3 from './assets/level3.png';
+import level4 from './assets/level4.png';
+import level5 from './assets/level5.png';
+import level6 from './assets/level6.png';
+import level7 from './assets/level7.png';
+import level8 from './assets/level8.png';
+import level9 from './assets/level9.png';
+import level10 from './assets/level10.png';
+import platform from './assets/platform.png';
+import dude from './assets/dude.png';
 export default class BootScene extends Phaser.Scene {
   constructor() {
     super({ key: 'boot' });
@@ -25,21 +36,18 @@ export default class BootScene extends Phaser.Scene {
     this.load.audio('fall', fall);
     this.load.audio('jump', jump);
     this.load.audio('land', land);
-    this.load.image('space', images.space);
-    this.load.image('logo', images.logo);
-    this.load.image('red', images.red);
-    this.load.image('level1', images.level1);
-    this.load.image('level2', images.level2);
-    this.load.image('level3', images.level3);
-    this.load.image('level4', images.level4);
-    this.load.image('level5', images.level5);
-    this.load.image('level6', images.level6);
-    this.load.image('level7', images.level7);
-    this.load.image('level8', images.level8);
-    this.load.image('level9', images.level9);
-    this.load.image('level10', images.level10);
-    this.load.image('platform', images.platform);
-    this.load.spritesheet('dude', images.dude, {
+    this.load.image('level1', level1);
+    this.load.image('level2', level2);
+    this.load.image('level3', level3);
+    this.load.image('level4', level4);
+    this.load.image('level5', level5);
+    this.load.image('level6', level6);
+    this.load.image('level7', level7);
+    this.load.image('level8', level8);
+    this.load.image('level9', level9);
+    this.load.image('level10', level10);
+    this.load.image('platform', platform);
+    this.load.spritesheet('dude', dude, {
       frameWidth: 32,
       frameHeight: 48,
     });
